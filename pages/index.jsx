bf0f645de5c44101c47ba59644093788a047288e
Muhammad Sufyan Patel <69216156+msufiyanpatel@users.noreply.github.com>
2026-03-15 22:28:54 +0500

    Upgrade framer-motion 8 to 12 to fix production black screen, add loading states to dynamic imports

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,25 @@
 import dynamic from 'next/dynamic';
 
-const Home = dynamic(() => import('../src/views/Home'), { ssr: false });
-const HomeServices = dynamic(() => import('../src/views/HomeServices'), { ssr: false });
-const Benefits = dynamic(() => import('../src/views/Benefits'), { ssr: false });
-const Locations = dynamic(() => import('../src/views/Locations'), { ssr: false });
-const Roadmap = dynamic(() => import('../src/views/Roadmap'), { ssr: false });
+const Home = dynamic(() => import('../src/views/Home'), {
+  ssr: false,
+  loading: () => null,
+});
+const HomeServices = dynamic(() => import('../src/views/HomeServices'), {
+  ssr: false,
+  loading: () => null,
+});
+const Benefits = dynamic(() => import('../src/views/Benefits'), {
+  ssr: false,
+  loading: () => null,
+});
+const Locations = dynamic(() => import('../src/views/Locations'), {
+  ssr: false,
+  loading: () => null,
+});
+const Roadmap = dynamic(() => import('../src/views/Roadmap'), {
+  ssr: false,
+  loading: () => null,
+});
 
 export default function IndexPage() {
   return (
